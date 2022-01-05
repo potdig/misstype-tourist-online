5ce9507b8e5d9afc5f6ae5ab85bb79c38bc7f7c3
potdig <potadig.ashnerd.zino@gmail.com>
2022-01-05 22:19:18 +0900

    save caption (main, sub) to replicant

--- dashboard/caption.js
+++ dashboard/caption.js
@@ -1,7 +1,28 @@
-const replicant = nodecg.Replicant("caption", {
-  defaultValue: "誤字ツーリストOnline"
+const form = document.getElementById('caption-form')
+const subForm = document.getElementById('sub-caption-form')
+
+const defaultCaption = {
+  main: "誤字ツーリストOnline",
+  sub: "ゲーム名"
+}
+
+const replicant = nodecg.Replicant('caption', {
+  defaultValue: defaultCaption
 })
+
 replicant.on('change', newCaption => {
-  const form = document.getElementById('caption-form')
-  form.value = newCaption
+  form.value = newCaption.main
+  subForm.value = newCaption.sub
 })
+
+const button = document.getElementById('apply-button')
+button.onclick = () => {
+  replicant.value = {
+    main: form.value,
+    sub: subForm.value
+  }
+}
+
+if (!replicant.value.main) {
+  replicant.value = defaultCaption
+}
